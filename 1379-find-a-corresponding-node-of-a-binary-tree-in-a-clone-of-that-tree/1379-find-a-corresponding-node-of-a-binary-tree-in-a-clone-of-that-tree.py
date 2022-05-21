fe59a4535e586c5138f25a52264da52ac1a48042
Aditya Gupta <guptaaditya008@gmail.com>
2022-05-21 21:04:39 +0530

    Time: 784 ms (59.62%), Space: 24 MB (68.70%) - LeetHub

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.py
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.py
@@ -7,24 +7,31 @@
 
 class Solution:
     def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
-        originalQueue = [original]
-        cloneQueue = [cloned]
         
-        while len(originalQueue) != 0 and len(cloneQueue) != 0:
-            origElement = originalQueue[0]
-            cloneElement = cloneQueue[0]
-            
-            if origElement == target:
-                return cloneElement
-            
-            if origElement.left: 
-                originalQueue.append(origElement.left)
-                cloneQueue.append(cloneElement.left)
-            if origElement.right: 
-                originalQueue.append(origElement.right)
-                cloneQueue.append(cloneElement.right)
-            
-            originalQueue.pop(0)
-            cloneQueue.pop(0)
+        if not original or original == target: return cloned
+        res = self.getTargetCopy(original.left, cloned.left, target)
+        if res: return res
+        return self.getTargetCopy(original.right, cloned.right, target)
         
-        return None
+        # BFS
+#         originalQueue = [original]
+#         cloneQueue = [cloned]
+        
+#         while len(originalQueue) != 0 and len(cloneQueue) != 0:
+#             origElement = originalQueue[0]
+#             cloneElement = cloneQueue[0]
+            
+#             if origElement == target:
+#                 return cloneElement
+            
+#             if origElement.left: 
+#                 originalQueue.append(origElement.left)
+#                 cloneQueue.append(cloneElement.left)
+#             if origElement.right: 
+#                 originalQueue.append(origElement.right)
+#                 cloneQueue.append(cloneElement.right)
+            
+#             originalQueue.pop(0)
+#             cloneQueue.pop(0)
+        
+#         return None
