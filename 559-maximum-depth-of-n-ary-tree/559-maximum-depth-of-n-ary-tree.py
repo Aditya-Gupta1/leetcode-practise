6fac19c8df76f8806d21af327999693f731ff7a7
Aditya Gupta <guptaaditya008@gmail.com>
2022-05-26 11:40:03 +0530

    Time: 76 ms (24.85%), Space: 16.1 MB (52.06%) - LeetHub

--- 559-maximum-depth-of-n-ary-tree/559-maximum-depth-of-n-ary-tree.py
+++ 559-maximum-depth-of-n-ary-tree/559-maximum-depth-of-n-ary-tree.py
@@ -8,9 +8,24 @@ class Node:
 
 class Solution:
     def maxDepth(self, root: 'Node') -> int:
+        
+        # DFS
+        # if not root: return 0
+        # maxHeight = 0
+        # for child in root.children:
+        #     height = self.maxDepth(child)
+        #     maxHeight = max(maxHeight, height)
+        # return maxHeight + 1
+        
+        # BFS
         if not root: return 0
-        maxHeight = 0
-        for child in root.children:
-            height = self.maxDepth(child)
-            maxHeight = max(maxHeight, height)
-        return maxHeight + 1
+        que = [root]
+        depth = 0
+        while len(que) > 0:
+            currLen = len(que)
+            for i in range(currLen):
+                node = que.pop(0)
+                for child in node.children:
+                    que.append(child)
+            depth += 1
+        return depth
