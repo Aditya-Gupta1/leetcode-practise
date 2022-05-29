53bb8b4374d3f92d7da2eb2e9e36cbd5093b188b
Aditya Gupta <guptaaditya008@gmail.com>
2022-05-29 12:59:43 +0530

    Time: 4073 ms (21.21%), Space: 14.5 MB (42.09%) - LeetHub

---
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.py
@@ -0,0 +1,16 @@
+class Solution:
+    def compare(self, word1, word2):
+        words = set(list(word1))
+        for ch in word2:
+            if ch in words: return False
+        return True
+    
+    def maxProduct(self, words: List[str]) -> int:
+        n = len(words)
+        maxProduct = 0
+        
+        for i in range(n):
+            for j in range(i+1, n):
+                if self.compare(words[i], words[j]):
+                    maxProduct = max(maxProduct, len(words[i])*len(words[j]))
+        return maxProduct
