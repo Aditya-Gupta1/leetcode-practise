379399854779c89d16c2507ca55a31558711c474
Aditya Gupta <guptaaditya008@gmail.com>
2022-05-31 11:04:27 +0530

    Time: 52 ms (32.61%), Space: 14.7 MB (44.27%) - LeetHub

--- 404-sum-of-left-leaves/404-sum-of-left-leaves.py
+++ 404-sum-of-left-leaves/404-sum-of-left-leaves.py
@@ -6,16 +6,31 @@
 #         self.right = right
 class Solution:
     
+    def helper(self, root):
+        if root.left:
+            if not root.left.left and not root.left.right:
+                self.total += root.left.val
+            self.helper(root.left)
+        if root.right: self.helper(root.right)
+    
     def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
-        total = 0
-        que = [root]
         
-        while len(que) > 0:
-            node = que.pop(0)
-            if node.left:
-                if not node.left.right and not node.left.left:
-                    total += node.left.val
-                que.append(node.left)
-            if node.right: que.append(node.right)
+        # BFS
+#         total = 0
+#         que = [root]
         
-        return total
+#         while len(que) > 0:
+#             node = que.pop(0)
+#             if node.left:
+#                 if not node.left.right and not node.left.left:
+#                     total += node.left.val
+#                 que.append(node.left)
+#             if node.right: que.append(node.right)
+        
+#         return total
+        # DFS
+        
+        self.total = 0
+        self.helper(root)
+        return self.total
+        
