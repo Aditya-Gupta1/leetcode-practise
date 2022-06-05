d4ce5df6dffc0f544430a1984c494da6e54fc7b2
Aditya Gupta <guptaaditya008@gmail.com>
2022-06-05 18:17:14 +0530

    Time: 82 ms (13.02%), Space: 15.2 MB (15.46%) - LeetHub

--- 112-path-sum/112-path-sum.py
+++ 112-path-sum/112-path-sum.py
@@ -7,9 +7,22 @@
 class Solution:
         
     def hasPathSum(self, root: Optional[TreeNode], target: int) -> bool:
+        # DFS
+        # if not root: return False
+        # if not root.left and not root.right:
+        #     if target == root.val: return True
+        #     else: return False
+        # if self.hasPathSum(root.left, target - root.val): return True
+        # else: return self.hasPathSum(root.right, target - root.val)
+        
+        # BFS
         if not root: return False
-        if not root.left and not root.right:
-            if target == root.val: return True
-            else: return False
-        if self.hasPathSum(root.left, target - root.val): return True
-        else: return self.hasPathSum(root.right, target - root.val)
+        que = [[root, target - root.val]]
+        while len(que) > 0:
+            node, value = que.pop(0)
+            if not node.left and not node.right and value == 0: return True
+            if node.left:
+                que.append([node.left, value - node.left.val])
+            if node.right:
+                que.append([node.right, value - node.right.val])
+        return False
