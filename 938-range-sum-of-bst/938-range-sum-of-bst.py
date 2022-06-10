65b675cbac3c54036dd7122da6e1f2f22888d183
Aditya Gupta <guptaaditya008@gmail.com>
2022-06-10 09:28:07 +0530

    Time: 354 ms (28.86%), Space: 23.4 MB (7.35%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.py
+++ 938-range-sum-of-bst/938-range-sum-of-bst.py
@@ -8,22 +8,24 @@ class Solution:
             
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
         # approach-1
-#         que = [root]
-#         total = 0
+        que = [root]
+        total = 0
         
-#         while que:
-#             node = que.pop(0)
-#             if low <= node.val <= high:
-#                 total += node.val
-#             if node.left: que.append(node.left)
-#             if node.right: que.append(node.right)
+        while que:
+            node = que.pop(0)
+            if not node: continue
+            if node.val > low: que.append(node.left)
+            if node.val < high: que.append(node.right)
+            if low <= node.val <= high:
+                total += node.val
         
-#         return total
+        return total
 
         # approach-2
-        if not root: return 0
-        if root.val < low:
-            return self.rangeSumBST(root.right, low, high)
-        if root.val > high:
-            return self.rangeSumBST(root.left, low, high)
-        return root.val + self.rangeSumBST(root.right, low, high) + self.rangeSumBST(root.left, low, high)
+        # if not root: return 0
+        # if root.val < low:
+        #     return self.rangeSumBST(root.right, low, high)
+        # if root.val > high:
+        #     return self.rangeSumBST(root.left, low, high)
+        # return root.val + self.rangeSumBST(root.right, low, high) + self.rangeSumBST(root.left, low, high)
+        
