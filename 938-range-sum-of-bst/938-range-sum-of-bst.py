33d2f2e60dd1e254125e5e53ddfe1daf429dd86a
Aditya Gupta <guptaaditya008@gmail.com>
2022-06-10 09:18:39 +0530

    Time: 224 ms (84.79%), Space: 23.1 MB (48.35%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.py
+++ 938-range-sum-of-bst/938-range-sum-of-bst.py
@@ -5,15 +5,33 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    
+    def helper(self, root, low, high):
+        if not root: return
+        if low <= root.val <= high:
+            self.total += root.val
+            self.helper(root.left, low, high)
+            self.helper(root.right, low, high)
+        elif root.val < low:
+            self.helper(root.right, low, high)
+        else:
+            self.helper(root.left, low, high)
+            
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
-        que = [root]
-        total = 0
+        # approach-1
+#         que = [root]
+#         total = 0
         
-        while que:
-            node = que.pop(0)
-            if low <= node.val <= high:
-                total += node.val
-            if node.left: que.append(node.left)
-            if node.right: que.append(node.right)
+#         while que:
+#             node = que.pop(0)
+#             if low <= node.val <= high:
+#                 total += node.val
+#             if node.left: que.append(node.left)
+#             if node.right: que.append(node.right)
         
-        return total
+#         return total
+
+        # approach-2
+        self.total = 0
+        self.helper(root, low, high)
+        return self.total
