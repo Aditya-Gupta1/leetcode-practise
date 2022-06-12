950367bf214bb9162ba7bd14ae36fd181fa4a040
Aditya Gupta <guptaaditya008@gmail.com>
2022-06-12 16:39:22 +0530

    Time: 47 ms (11.89%), Space: 13.8 MB (99.02%) - LeetHub

--- 897-increasing-order-search-tree/897-increasing-order-search-tree.py
+++ 897-increasing-order-search-tree/897-increasing-order-search-tree.py
@@ -5,16 +5,30 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def inorder(self, root):
+    # def inorder(self, root):
+    #     if root:
+    #         yield from self.inorder(root.left)
+    #         yield root.val
+    #         yield from self.inorder(root.right)
+    
+    def inorder2(self, root):
         if root:
-            yield from self.inorder(root.left)
-            yield root.val
-            yield from self.inorder(root.right)
+            self.inorder2(root.left)
+            self.curr.right = root
+            root.left = None
+            self.curr = self.curr.right
+            self.inorder2(root.right)
             
     def increasingBST(self, root: TreeNode) -> TreeNode:
         # approach-1
-        ans = curr = TreeNode(None)
-        for node in self.inorder(root):
-            curr.right = TreeNode(node)
-            curr = curr.right
+        # ans = curr = TreeNode(None)
+        # for node in self.inorder(root):
+        #     curr.right = TreeNode(node)
+        #     curr = curr.right
+        # return ans.right
+        
+        # approach-2
+        self.curr = ans = TreeNode(None)
+        self.inorder2(root)
         return ans.right
+        
