a1f3a34c83ff5490afa172ded4101881a0c20a52
Aditya Gupta <guptaaditya008@gmail.com>
2022-06-18 18:51:48 +0530

    Time: 62 ms (35.31%), Space: 14.3 MB (7.97%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.py
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.py
@@ -5,14 +5,28 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def util(self, root, currNum = "", total = 0):
-        if not root: return total
-        if not root.right and not root.left:
-            total += int(currNum + str(root.val), 2)
-            return total
-        left = self.util(root.left, currNum + str(root.val), total)
-        right = self.util(root.right, currNum + str(root.val), total)
-        return left + right
+    # def util(self, root, currNum = "", total = 0):
+    #     if not root: return total
+    #     if not root.right and not root.left:
+    #         total += int(currNum + str(root.val), 2)
+    #         return total
+    #     left = self.util(root.left, currNum + str(root.val), total)
+    #     right = self.util(root.right, currNum + str(root.val), total)
+    #     return left + right
     
     def sumRootToLeaf(self, root: Optional[TreeNode]) -> int:
-        return self.util(root)
+        # Approach 1
+        # return self.util(root)
+        
+        # Approach 2
+        que = [[root, ""]]
+        total = 0
+        while que:
+            node, num = que.pop(0)
+            if not node.left and not node.right:
+                total += int(num + str(node.val), 2)
+            if node.left:
+                que.append([node.left, num + str(node.val)])
+            if node.right:
+                que.append([node.right, num + str(node.val)])
+        return total
