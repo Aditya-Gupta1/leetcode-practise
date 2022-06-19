7109515ecbdc4bd87fafee25be9700ecdc5cdea9
Aditya Gupta <guptaaditya008@gmail.com>
2022-06-19 11:33:47 +0530

    Time: 35 ms (88.51%), Space: 13.8 MB (75.06%) - LeetHub

--- 257-binary-tree-paths/257-binary-tree-paths.py
+++ 257-binary-tree-paths/257-binary-tree-paths.py
@@ -5,16 +5,31 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def findPaths(self, root, path = ""):
-        if not root: return
-        if not root.right and not root.left:
-            path += str(root.val)
-            self.ans.append(path)
-            return
-        self.findPaths(root.left, path + str(root.val) + "->")
-        self.findPaths(root.right, path + str(root.val) + "->")
+    # def findPaths(self, root, path = ""):
+    #     if not root: return
+    #     if not root.right and not root.left:
+    #         path += str(root.val)
+    #         self.ans.append(path)
+    #         return
+    #     self.findPaths(root.left, path + str(root.val) + "->")
+    #     self.findPaths(root.right, path + str(root.val) + "->")
         
     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
-        self.ans = []
-        self.findPaths(root)
-        return self.ans
+        # approach 1
+        # self.ans = []
+        # self.findPaths(root)
+        # return self.ans
+        
+        # approach 2
+        que = [[root, ""]]
+        ans = []
+        while que:
+            node, path = que.pop(0)
+            if not node.left and not node.right:
+                path += str(node.val)
+                ans.append(path)
+            if node.left:
+                que.append([node.left, path + str(node.val) + "->"])
+            if node.right:
+                que.append([node.right, path + str(node.val) + "->"])
+        return ans
