7b30701c38821f1eef4064ff172285ec5a998af1
Aditya Gupta <guptaaditya008@gmail.com>
2022-06-24 12:22:20 +0530

    Time: 194 ms (39.32%), Space: 17 MB (5.02%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.py
@@ -4,6 +4,7 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from hashlib import sha256
 class Solution:
     def isIdentical(self, root1, root2):
         if not root1 and not root2: return True
@@ -15,14 +16,39 @@ class Solution:
             else: return False
         return False
     
+    def hash(self, value):
+        s = sha256()
+        value = value.encode('utf-8')
+        s.update(value)
+        return s.hexdigest()
+    
+    def merkle(self, root):
+        if not root: return '#'
+        left = self.merkle(root.left)
+        right = self.merkle(root.right)
+        root.merkle = self.hash(left + str(root.val) + right)
+        return root.merkle
+    
+    def dfs(self, root, subRoot):
+        if not root: return False
+        return root.merkle == subRoot.merkle or self.dfs(root.right, subRoot) or self.dfs(root.left, subRoot)
+    
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        if not root and not subRoot: return True
-        que = [root]
-        ans = []
-        while que:
-            node = que.pop(0)
-            if node.val == subRoot.val:
-                ans.append(self.isIdentical(node, subRoot))
-            if node.left: que.append(node.left)
-            if node.right: que.append(node.right)
-        return any(ans)
+        
+        # approach 1
+        # if not root and not subRoot: return True
+        # que = [root]
+        # ans = []
+        # while que:
+        #     node = que.pop(0)
+        #     if node.val == subRoot.val:
+        #         ans.append(self.isIdentical(node, subRoot))
+        #     if node.left: que.append(node.left)
+        #     if node.right: que.append(node.right)
+        # return any(ans)
+        
+        # approach 2
+        self.merkle(root)
+        self.merkle(subRoot)
+        
+        return self.dfs(root, subRoot)
