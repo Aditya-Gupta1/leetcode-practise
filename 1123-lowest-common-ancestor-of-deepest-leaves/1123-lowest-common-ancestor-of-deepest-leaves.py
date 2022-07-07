c35130a3f0d9dd234eab2258512467d4b340801c
Aditya Gupta <guptaaditya008@gmail.com>
2022-07-07 11:04:10 +0530

    Time: 86 ms (34.47%), Space: 14.5 MB (9.56%) - LeetHub

--- 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.py
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.py
@@ -1,30 +1,45 @@
 class Solution:
-    def lca(self, root, nodes):
-        if not root: return None
-        if root in nodes: return root
-        left = self.lca(root.left, nodes)
-        right = self.lca(root.right, nodes)
-        if not left: return right
-        if not right: return left
-        return root
+    # def lca(self, root, nodes):
+    #     if not root: return None
+    #     if root in nodes: return root
+    #     left = self.lca(root.left, nodes)
+    #     right = self.lca(root.right, nodes)
+    #     if not left: return right
+    #     if not right: return left
+    #     return root
     
     def lcaDeepestLeaves(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        if not root.left and not root.right: return root
         
-        maxDepth = 0
-        deepestLeaves = []
-        que = [[root, 0]]
+        # brute force
+#         if not root.left and not root.right: return root
         
-        while que:
-            node, depth = que.pop(0)
-            if depth > maxDepth:
-                deepestLeaves = [node]
-                maxDepth = depth
-            elif depth == maxDepth:
-                deepestLeaves.append(node)
-            if node.left: que.append([node.left, depth+1])
-            if node.right: que.append([node.right, depth+1])
+#         maxDepth = 0
+#         deepestLeaves = []
+#         que = [[root, 0]]
         
-        if len(deepestLeaves) == 1: return deepestLeaves[0]
+#         while que:
+#             node, depth = que.pop(0)
+#             if depth > maxDepth:
+#                 deepestLeaves = [node]
+#                 maxDepth = depth
+#             elif depth == maxDepth:
+#                 deepestLeaves.append(node)
+#             if node.left: que.append([node.left, depth+1])
+#             if node.right: que.append([node.right, depth+1])
         
-        return self.lca(root, deepestLeaves)
+        # if len(deepestLeaves) == 1: return deepestLeaves[0]
+        
+#         return self.lca(root, deepestLeaves)
+        
+        # approach 2
+        def helper(root):
+            if not root: return [0, root]
+            leftHeight, leftLCA = helper(root.left)
+            rightHeight, rightLCA = helper(root.right)
+            if leftHeight > rightHeight:
+                return [leftHeight+1, leftLCA]
+            if rightHeight > leftHeight:
+                return [rightHeight+1, rightLCA]
+            return [leftHeight+1, root]
+        
+        return helper(root)[1]
