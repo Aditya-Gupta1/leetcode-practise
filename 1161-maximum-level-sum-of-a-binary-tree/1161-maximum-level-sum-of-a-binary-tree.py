a03f010bcdfbb17d845e7c4f698546788b6c79ff
Aditya Gupta <guptaaditya008@gmail.com>
2022-07-11 10:11:31 +0530

    Time: 732 ms (5.08%), Space: 18.3 MB (76.99%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.py
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.py
@@ -6,28 +6,45 @@
 #         self.right = right
 class Solution:
     def maxLevelSum(self, root: Optional[TreeNode]) -> int:
-        ansDepth = 0
-        maxSum = float('-inf')
+        # approach-1: BFS
+#         ansDepth = 0
+#         maxSum = float('-inf')
         
-        que = [root]
-        depth = 0
+#         que = [root]
+#         depth = 0
         
-        while que:
+#         while que:
             
-            currSize = len(que)
-            total = 0
+#             currSize = len(que)
+#             total = 0
             
-            for _ in range(currSize):
-                node = que.pop(0)
-                total += node.val
+#             for _ in range(currSize):
+#                 node = que.pop(0)
+#                 total += node.val
                 
-                if node.left: que.append(node.left)
-                if node.right: que.append(node.right)
+#                 if node.left: que.append(node.left)
+#                 if node.right: que.append(node.right)
             
-            depth += 1
+#             depth += 1
             
-            if total > maxSum:
-                maxSum = total
-                ansDepth = depth
+#             if total > maxSum:
+#                 maxSum = total
+#                 ansDepth = depth
         
-        return ansDepth
+#        return ansDepth
+        
+        # approach-2: DFS
+        def dfs(root, totals, level):
+            if not root: return
+            
+            if len(totals) == level:
+                totals.append(root.val)
+            else:
+                totals[level] += root.val
+            
+            dfs(root.left, totals, level+1)
+            dfs(root.right, totals, level+1)
+        
+        totals = []
+        dfs(root, totals, 0)
+        return 1 + totals.index(max(totals))
