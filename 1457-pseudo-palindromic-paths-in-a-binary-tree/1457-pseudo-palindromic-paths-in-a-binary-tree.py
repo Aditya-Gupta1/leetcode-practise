89931091801b3f89652cb59698c4285f6a004e7c
Aditya Gupta <guptaaditya008@gmail.com>
2022-07-11 09:51:30 +0530

    Time: 1209 ms (64.26%), Space: 85.8 MB (50.86%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.py
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.py
@@ -5,30 +5,15 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def helper(self, root, freq):
-        if not root: return
-        if not root.right and not root.left:
-            freq[root.val] = freq.get(root.val, 0) + 1
-            
-            nOdds = 0
-            for k, v in freq.items():
-                if v % 2 != 0: nOdds += 1
-            if nOdds <= 1: self.ans += 1
-            
-            freq[root.val] -= 1
-            
-            return
-        
-        freq[root.val] = freq.get(root.val, 0) + 1
-        
-        self.helper(root.left, freq)
-        self.helper(root.right, freq)
-        
-        freq[root.val] -= 1
-        
-        return
     
-    def pseudoPalindromicPaths (self, root: Optional[TreeNode]) -> int:
-        self.ans = 0
-        self.helper(root, dict())
-        return self.ans
+    def pseudoPalindromicPaths (self, root: Optional[TreeNode], count = 0) -> int:
+        if not root: return 0
+        
+        count ^= 1 << (root.val - 1)
+        
+        res = self.pseudoPalindromicPaths(root.left, count) + self.pseudoPalindromicPaths(root.right, count)
+        
+        if not root.right and not root.left:
+            if count & (count - 1) == 0: res += 1
+        
+        return res
