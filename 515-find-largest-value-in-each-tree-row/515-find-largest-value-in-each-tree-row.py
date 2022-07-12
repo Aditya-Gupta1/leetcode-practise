ac5b6dd7a7736b4de8f2aa5411c698c0c06c7656
Aditya Gupta <guptaaditya008@gmail.com>
2022-07-12 09:29:20 +0530

    Time: 93 ms (18.91%), Space: 17.1 MB (12.06%) - LeetHub

--- 515-find-largest-value-in-each-tree-row/515-find-largest-value-in-each-tree-row.py
+++ 515-find-largest-value-in-each-tree-row/515-find-largest-value-in-each-tree-row.py
@@ -5,20 +5,35 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def helper(self, root, result, depth):
+        if not root: return
+        
+        if len(result) == depth: result.append(root.val)
+        else: result[depth] = max(result[depth], root.val)
+            
+        self.helper(root.left, result, depth+1)
+        self.helper(root.right, result, depth+1)
+        
     def largestValues(self, root: Optional[TreeNode]) -> List[int]:
-        if not root: return []
+        # approach 1
+#         if not root: return []
         
-        ans = []
-        que = [root]
+#         ans = []
+#         que = [root]
         
-        while que:
-            currSize = len(que)
-            maxEle = float('-inf')
-            for _ in range(currSize):
-                node = que.pop(0)
-                maxEle = max(maxEle, node.val)
-                if node.right: que.append(node.right)
-                if node.left: que.append(node.left)
-            ans.append(maxEle)
+#         while que:
+#             currSize = len(que)
+#             maxEle = float('-inf')
+#             for _ in range(currSize):
+#                 node = que.pop(0)
+#                 maxEle = max(maxEle, node.val)
+#                 if node.right: que.append(node.right)
+#                 if node.left: que.append(node.left)
+            # ans.append(maxEle)
         
-        return ans
+#         return ans
+        # approach-2
+        result = []
+        depth = 0
+        self.helper(root, result, depth)
+        return result
