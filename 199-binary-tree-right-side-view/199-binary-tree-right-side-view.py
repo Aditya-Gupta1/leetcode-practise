38d3104cf43037a963a3019a5ea72f8fa763f51b
Aditya Gupta <guptaaditya008@gmail.com>
2022-07-12 10:54:57 +0530

    Time: 47 ms (59.51%), Space: 13.8 MB (69.78%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
@@ -6,9 +6,24 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        res = []
-        self.helper(root, res, 0)
-        return res
+        if not root: return []
+        
+        que = [[root, 0]]
+        ans = []
+        
+        while que:
+            node, depth = que.pop(0)
+            if depth == len(ans): ans.append(node.val)
+            if node.right: que.append((node.right, depth+1))
+            if node.left: que.append((node.left, depth+1))
+                
+        return ans
+        
+        
+        # approach-1: DFS
+#         res = []
+#         self.helper(root, res, 0)
+#         return res
     
     def helper(self, root, res, level):
         if not root: return
