aa298cf487baad41f1e519a01466d1ef4df42783
Aditya Gupta <guptaaditya008@gmail.com>
2022-07-14 10:57:24 +0530

    Time: 2413 ms (29.46%), Space: 45.8 MB (48.41%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
@@ -1,33 +1,18 @@
-from collections import defaultdict
-class TreeNode:
-    def __init__(self, val, children = None):
-        self.val = val
-        self.children = [] if children == None else children
-        
+      
 class Solution:
-    def constructTree(self, headID, managers, informTime, n):
-        nodes = dict()
-        for i in range(n):
-            nodes[i] = TreeNode(i)
-        
-        for employee, manager in enumerate(managers):
-            if manager != -1:
-                nodes[manager].children.append(nodes[employee])
-        
-        return nodes[headID]
-    
-    def numOfMinutesUtil(self, root, informTime):
-        if not root: return 0
-        
-        maxTime = 0
-        for child in root.children:
-            time = self.numOfMinutesUtil(child, informTime)
-            maxTime = max(maxTime, time)
-        
-        return maxTime + informTime[root.val]
     
     def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
         
-        root = self.constructTree(headID, manager, informTime, n)
-        return self.numOfMinutesUtil(root, informTime)
+        children = [[] for _ in range(n)]
+        for emp, man in enumerate(manager):
+            if man != -1:
+                children[man].append(emp)
         
+        def dfs(emp):
+            maxTime = 0
+            for child in children[emp]:
+                time = dfs(child)
+                maxTime = max(maxTime, time)
+            return maxTime + informTime[emp]
+        
+        return dfs(headID)
